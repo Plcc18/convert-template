7c5ee3820b2e3b1760213f32cca9e214ba5be2d1
Plcc18 <pedrolucascabral4@gmail.com>
2024-11-03 15:04:52 -0300

    identificando moeda

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
+// Cotação de moeda
+const USD = 5.70
+const EUR = 6.20
+const GBP = 7.60
+
 // Obtendo elementos do formulário
 const form = document.querySelector("form")
 const amount = document.getElementById("amount")
@@ -13,5 +18,23 @@ amount.addEventListener("input", () => {
 form.onsubmit = (event) => {
   event.preventDefault()
 
-  console.log(currency.value)
+  switch (currency.value) {
+    case "USD":
+      convertCurency(amount.value, USD, "US$");
+      break;
+
+    case "EUR":
+      convertCurency(amount.value, EUR, "€");
+      break;
+
+    case "GBP":
+      convertCurency(amount.value, GBP, "£");
+      break;
+  }
+
+}
+
+// Função para converter a moeda
+function convertCurency (amount, price,symbol) {
+  console.log(amount, price, symbol)
 }
